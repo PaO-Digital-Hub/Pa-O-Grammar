ec398131d3f5e2fdf9d6887b7c4726da7db89078
PaO-Digital-Hub <pao.digital.hub@gmail.com>
2026-09-13 22:52:40 +0700

    Create universal-features.js

---
+++ src/ud/universal-features.js
@@ -0,0 +1,7 @@
+export const UFEATURES = {
+  Aspect: ['Imp', 'Perf', 'Prog'],
+  Case: ['Nom', 'Acc', 'Dat', 'Gen'],
+  Number: ['Sing', 'Plur'],
+  Person: ['1', '2', '3'],
+  Tense: ['Past', 'Pres', 'Fut']
+};

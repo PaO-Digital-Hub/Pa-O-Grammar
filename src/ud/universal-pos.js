31ff55f0f6217aaacd430426751227bf20d60131
PaO-Digital-Hub <pao.digital.hub@gmail.com>
2026-09-13 22:51:26 +0700

    Create universal-pos.js

---
+++ src/ud/universal-pos.js
@@ -0,0 +1,5 @@
+export const UPOS = {
+  NOUN: 'NOUN', PRON: 'PRON', VERB: 'VERB', ADJ: 'ADJ',
+  ADV: 'ADV', ADP: 'ADP', CCONJ: 'CCONJ', DET: 'DET',
+  AUX: 'AUX', NUM: 'NUM', PART: 'PART', PUNCT: 'PUNCT'
+};
